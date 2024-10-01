bfa42cc7b0db737e2575cac42bffba6ba3f38f78
Dmitriy Remezov <darknessdizi@gmail.com>
2024-10-01 18:26:48 +0700

    Страница регистрации переведена на локальное хранилище

--- src/components/Pages/RegistrationPage/RegistrationPage.tsx
+++ src/components/Pages/RegistrationPage/RegistrationPage.tsx
@@ -6,30 +6,41 @@ import "./registrationPage.css";
 import { useState } from 'react';
 import { checkEmail, checkLogin, checkPassword } from "./utils";
 
-const typeErrors = {
-  login: { status: false, message: '' },
-  password: { status: false, message: '' },
-  email: { status: false, message: '' },
+const initialState = {
+  errorLogin: { status: false, message: '' },
+  errorPassword: { status: false, message: '' },
+  errorRepeat: { status: false, message: '' },
+  errorEmail: { status: false, message: '' },
+  login: '',
+  password: '',
+  repeat: '',
+  email: '',
+  fullName: '',
 }
 
 export const RegistrationPage = () => {
-  const [statusErrors, setStatusErrors] = useState(typeErrors);
+  const [statePage, setStatePage] = useState(initialState);
   const { user } = useAppSelector((state) => state.identification); // хук useAppSelector принимает callback
   const dispatch = useAppDispatch(); // dispatch это словно диспетчер - он доставляет action для нашего редьюсера
 
   const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
-    // Обрабатываем отправку формы
+    // Обрабатываем отправку формы регистрации
     event.preventDefault();
+    const { login, password, repeat, email } = event.target;
     const errors = {
-      login: checkLogin(event.target.login.value),
-      password: checkPassword(event.target.password.value),
-      email: checkEmail(event.target.email.value),
+      errorLogin: checkLogin(login.value),
+      errorPassword: checkPassword(password.value),
+      errorRepeat: (repeat.value === password.value) ? initialState.errorRepeat : { status: true, message: 'Ошибка в пароле' },
+      errorEmail: checkEmail(email.value),
     }
 
-    if ((!errors.login.status) && (!errors.password.status) && (!errors.email.status)) {
+    setStatePage({ 
+      ...statePage,
+      ...errors
+    });
+
+    if ((!errors.errorLogin.status) && (!errors.errorPassword.status) && (!errors.errorRepeat.status) && (!errors.errorEmail.status)) {
       dispatch(registrationUser(user));
-    } else {
-      setStatusErrors({ ...errors });
     }
   }
 
@@ -39,7 +50,11 @@ export const RegistrationPage = () => {
     const statusSearch = ['fullName', 'login', 'password', 'email', 'repeat'].includes(name);
 
     if (statusSearch) {
-      dispatch(changeUserParams({name, value}));
+      // dispatch(changeUserParams({name, value}));
+      setStatePage({ 
+        ...statePage,
+        [name]: value,
+      });
     }
   }
 
@@ -51,41 +66,44 @@ export const RegistrationPage = () => {
           title={"Логин"} 
           type={"text"} 
           name={"login"} 
-          value={user.login || ""} 
+          value={statePage.login} 
           changeInput={changeInput} 
-          message={statusErrors.login.message} 
-          error={statusErrors.login.status} 
+          message={statePage.errorLogin.message} 
+          error={statePage.errorLogin.status} 
         />
         <ItemLabel 
           title={"Пароль"} 
           type={"password"} 
           name={"password"} 
-          value={user.password || ""} 
+          value={statePage.password} 
           changeInput={changeInput} 
-          message={statusErrors.password.message} 
-          error={statusErrors.password.status} 
+          message={statePage.errorPassword.message} 
+          error={statePage.errorPassword.status} 
         />
         <ItemLabel 
           title={"Повторите пароль"} 
-          type={"password"} name={"repeat"} 
-          value={user.repeat || ""} 
-          changeInput={changeInput} 
+          type={"password"} 
+          name={"repeat"} 
+          value={statePage.repeat} 
+          changeInput={changeInput}
+          message={statePage.errorRepeat.message} 
+          error={statePage.errorRepeat.status}
         />
         <ItemLabel 
           title={"Полное имя"} 
           type={"text"} 
           name={"fullName"} 
-          value={user.fullName || ""} 
+          value={statePage.fullName} 
           changeInput={changeInput} 
         />
         <ItemLabel 
           title={"Электронная почта"} 
           type={"email"} 
           name={"email"} 
-          value={user.email || ""} 
+          value={statePage.email} 
           changeInput={changeInput} 
-          message={statusErrors.email.message} 
-          error={statusErrors.email.status} 
+          message={statePage.errorEmail.message} 
+          error={statePage.errorEmail.status} 
         />
       </ItemForm>
       <div className="conteiner__form__description">
