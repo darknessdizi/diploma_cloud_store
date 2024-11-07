c1e40c6e964ecee90f5df443a1786fe24fd5329b
Dmitriy Remezov <darknessdizi@gmail.com>
2024-11-07 14:00:52 +0700

    в URL добавлен ip адрес с http

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,5 +1,4 @@
-// export const URL_SERVER = 'https://91.197.96.56:8000';
-export const URL_SERVER = '91.197.96.56:8000';
+export const URL_SERVER = 'http://91.197.96.56:8000';
 
 export const MY_PATH = {
   root: '/',
